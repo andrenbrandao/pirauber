b7664f8b7e2d0575b06091d5693805e3b564449c
André Brandão <andrenbrandao@gmail.com>
2019-09-03 19:29:22 -0300

    Change DB to PostgreSQL

--- pirauber_project/settings.py
+++ pirauber_project/settings.py
@@ -82,11 +82,9 @@ WSGI_APPLICATION = 'pirauber_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    "default": env.db("DATABASE_URL", default="postgres:///pirauber")
 }
 
 
