abf8b896cd1044a5a401711cafdbf9af4093f5cd
André Brandão <andrenbrandao@gmail.com>
2019-09-28 17:12:46 -0300

    Add more fields to rides list

--- rides/tables.py
+++ rides/tables.py
@@ -4,9 +4,16 @@ from .models import Ride
 
 class RideTable(tables.Table):
     date = tables.DateTimeColumn(format='d M Y')
+    description = tables.Column()
 
     class Meta:
         model = Ride
         template_name = "django_tables2/bootstrap4.html"
-        fields = ('date', 'origin',
-                  'destination', 'driver')
+        fields = ('date', 'time', 'origin',
+                  'destination', 'driver', 'description', 'seats', 'price')
+        attrs = {"class": "table table-hover"}
+
+    def render_description(self, value, record):
+        if len(value) > 30:
+            return f"{value[:30]}..."
+        return value
