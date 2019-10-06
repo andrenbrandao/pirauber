dfd07fd4a0717853f471d4b5ffc81d8014f9fd1a
André Brandão <andrenbrandao@gmail.com>
2019-10-06 13:54:32 -0300

    Add Ride delete

--- rides/urls.py
+++ rides/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import RideListView, RideCreateView, RideUpdateView
+from .views import RideListView, RideCreateView, RideUpdateView, RideDeleteView
 
 urlpatterns = [
     path('', RideListView.as_view(), name='ride_list'),
     path('new/', RideCreateView.as_view(), name='ride_new'),
     path('<int:pk>/edit/', RideUpdateView.as_view(), name='ride_edit'),
+    path('<int:pk>/delete/', RideDeleteView.as_view(), name='ride_delete'),
 ]
--- rides/views.py
+++ rides/views.py
@@ -1,13 +1,40 @@
 import datetime
-from django.views.generic import CreateView, UpdateView, ListView
+from django.views.generic import CreateView, UpdateView, ListView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
 
-import django_tables2 as tables
+from django.http import JsonResponse
 
 from .models import Ride
 from .tables import RideTable
 from .forms import RideForm
+
+
+class AjaxableResponseMixin:
+    """
+    Mixin to add AJAX support to a form.
+    Must be used with an object-based FormView (e.g. CreateView)
+    """
+
+    def form_invalid(self, form):
+        response = super().form_invalid(form)
+        if self.request.is_ajax():
+            return JsonResponse(form.errors, status=400)
+        else:
+            return response
+
+    def form_valid(self, form):
+        # We make sure to call the parent's form_valid() method because
+        # it might do some processing (in the case of CreateView, it will
+        # call form.save() for example).
+        response = super().form_valid(form)
+        if self.request.is_ajax():
+            data = {
+                'pk': self.object.pk,
+            }
+            return JsonResponse(data)
+        else:
+            return response
 
 
 class RideListView(ListView):
@@ -33,6 +60,16 @@ class RideUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Ride
     form_class = RideForm
     template_name = "rides/ride_edit.html"
+    success_url = reverse_lazy('ride_list')
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj.driver == self.request.user
+
+
+class RideDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Ride
+    template_name = "rides/ride_delete.html"
     success_url = reverse_lazy('ride_list')
 
     def test_func(self):
